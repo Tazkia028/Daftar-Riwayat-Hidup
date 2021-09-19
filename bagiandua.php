7129e38335bc7513a2728ff677dc83c869049cdf
Tazkia028 <90827130+Tazkia028@users.noreply.github.com>
2021-09-19 13:00:24 +0700

    Add files via upload

---
+++ bagiandua.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html>
+    <head>
+        <title> Daftar Riwayat Hidup </title>
+    <body>
+    <style>
+    .border {
+     border: 4px solid red;
+    }
+    </style>
+    </head>
+    <img src="\Foto Tazkia.jpeg" alt="" width="300px" height="200px">
+
+        <?php
+        $nama = "Tazkia Ananda Oktavioleta";
+        $nim = 192520028;
+        $alamat = "Desa Kepuh, Rt 01 Rw 01, Kec.Kutoarjo, Purworejo";
+        $email = "tazkiaanandaaa@gmail.com";
+        $minat = "Multimedia"; 
+        ?>
+    
+        <p>Daftar Riwayat Hidup <br> </p>
+        <p> Nama : <?=$nama?> <br> </p>
+        <P> NIM : <?=$nim?> <br> </p>
+        <P> Alamat : <?=$alamat?> <br> </p> 
+        <P> Email : <?=$email?> <br> </p>
+        <P> Bidang minat : <?=$minat?> <br> </p>
+    </body>
+</html> 
+<?php
